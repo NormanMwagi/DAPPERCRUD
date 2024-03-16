2b2884cf2540f921bc068b46333dee49c93a58c5
Norman Mwagi <normanmwagi23@gmail.com>
2024-03-16 18:03:29 +0300

    Add project files.

---
+++ DAL/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace DAL.DataAccess
+{
+	public interface ISqlDataAccess
+	{
+		Task<IEnumerable<T>> GetAll<T, P>(string spName, P parameters, string connectionStr = "Conn");
+		Task AddData<T>(string spName, T parameters, string connectionStr = "Conn");
+	}
+}
---
+++ DAL/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DataAccess
+{
+	public class SqlDataAccess : ISqlDataAccess
+	{
+		private readonly IConfiguration config;
+
+		public SqlDataAccess(IConfiguration _config)
+		{
+			config = _config;
+		}
+
+		public async Task<IEnumerable<T>> GetAll<T, P>(string spName, P parameters, string connectionStr = "Conn")
+		{
+			var connstring = config.GetConnectionString(connectionStr);
+			using IDbConnection dbConnection = new SqlConnection(connstring);
+
+			return await dbConnection.QueryAsync<T>(spName, parameters, commandType: CommandType.StoredProcedure);
+		}
+
+		public async Task AddData<T>(string spName, T parameters, string connectionStr = "Conn")
+		{
+			var connstring = config.GetConnectionString(connectionStr);
+			using IDbConnection dbConnection = new SqlConnection(connstring);
+
+			await dbConnection.ExecuteAsync(spName, parameters, commandType: CommandType.StoredProcedure);
+		}
+
+	}
+}
